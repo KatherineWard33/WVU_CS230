c712d7015af1dcb9435b79834ba46a26abf468dc
KatherineWard33 <kmw0027@mix.wvu.edu>
2021-09-03 11:41:39 -0400

    topbar and footer updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Footer/footer.component';
+import { TopbarComponent } from './Header/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
